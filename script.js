ab8323e5d1e3d6ef916f7ed39778f6aa57dd9f1a
fazila04khan <khanfazila03@gmail.com>
2025-05-25 13:02:21 +0530

    Upload Project

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('Password').addEventListener('click', () => {
+  const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()?_";
+  const length = 12;
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+
+  document.getElementById('passwordOutput').textContent = password;
+});
